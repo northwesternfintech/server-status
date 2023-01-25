df5cb4dd142efbf9db4f5f1e8a52354420e62986
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-01-25 00:36:58 -0600

    implemented ESLint to package.json + setup
    added eslint, semicolons, and removed unused var

--- frontend/Components/UselessComponent.tsx
+++ frontend/Components/UselessComponent.tsx
@@ -11,10 +11,10 @@ export const UselessComponent: FunctionComponent = () => {
     const [state, setState] = useState(false);
 
     return (
-        <div onClick={(_) => setState(!state)} className={` ${state ? "text-3xl bg-red-300" : "text-2xl bg-blue-300"} font-bold underline`}>
+        <div onClick={() => setState(!state)} className={` ${state ? "text-3xl bg-red-300" : "text-2xl bg-blue-300"} font-bold underline`}>
             <span>I am {state ? "big" : "small"} text. Click me to change me.</span>
         </div>
     );
-}
+};
 
 export default UselessComponent;
