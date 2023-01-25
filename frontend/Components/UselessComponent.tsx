8f036e16cf0b18ccbd7efd53af87674584140a3e
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-01-25 00:14:12 -0600

    nextjs setup with uselesscomponent
    UselessComponent.tsx is a mini template that's... useless

---
+++ frontend/Components/UselessComponent.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent, useState } from "react";
+
+// Useless Component: does nothing
+/**
+ * Props:
+ * [param1: Type]: does nothing
+ * [param2: Type]: does nothing
+ * [param3: Type]: does nothing
+ */
+export const UselessComponent: FunctionComponent = () => {
+    const [state, setState] = useState(false);
+
+    return (
+        <div onClick={(_) => setState(!state)} className={` ${state ? "text-3xl bg-red-300" : "text-2xl bg-blue-300"} font-bold underline`}>
+            <span>I am {state ? "big" : "small"} text. Click me to change me.</span>
+        </div>
+    );
+}
+
+export default UselessComponent;
